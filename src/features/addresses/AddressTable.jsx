5878054cc95b7fc9061a50c69da892129d2914af
wjohns <wjohnstonjr@gmail.com>
2026-08-10 09:58:31 -0400

    Use Open Street Maps for the base style. Add runway labels. Use the runway color provided by the Martin service

--- src/features/addresses/AddressTable.jsx
+++ src/features/addresses/AddressTable.jsx
@@ -7,7 +7,7 @@ import { Layer, Map, NavigationControl, Source } from '@vis.gl/react-maplibre';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import PopupState, { bindMenu, bindTrigger } from 'material-ui-popup-state';
-import React from 'react';
+import React, { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Address from './Address';
 import { useGetAddressesQuery } from './AddressApi';
@@ -15,6 +15,20 @@ import { remove as deleteAddress } from './AddressService';
 import { setDialogOpen, setSelected, setSelections } from './AddressSlice';
 
 const AddressTable = () => {
+    const mapRef = useRef(null);
+    const getRawTileData = () => {
+        if (!mapRef.current) return;
+        const map = mapRef.current.getMap();
+
+        // Query directly from the raw vector tile cache
+        const allFeaturesInLoadedTiles = map.queryRenderedFeatures();
+
+        // map over features to extract your backend payload/properties
+        const rawProperties = allFeaturesInLoadedTiles.map(f => f.properties);
+
+        console.log('All raw properties from loaded service tiles:', rawProperties);
+    };
+
     const { data, isLoading, isError, refetch } = useGetAddressesQuery();
     const addresses = useSelector((state) => state.addresses?.list);
     const selections = useSelector((state) => state.addresses?.selections);
@@ -43,15 +57,6 @@ const AddressTable = () => {
 
     }
 
-    const parksLayer = {
-        id: 'parks',
-        type: 'fill',
-        'source-layer': 'park',
-        paint: {
-            'fill-color': '#00ff00',
-            'fill-opacity': 0.6
-        }
-    };
     const waterLayer = {
         id: 'water',
         type: 'fill',
@@ -61,22 +66,27 @@ const AddressTable = () => {
             'fill-opacity': 0.6
         }
     };
-    const transportationLayer = {
-        id: 'transportation_name',
-        type: 'line',
-        'source-layer': 'transportation_name',
-        paint: {
-            'line-color': '#ff0000',
-            'line-width': 2
-        }
-    };
     const runwaysLayer = {
         id: 'runways',
         type: 'fill',
         'source-layer': 'runways',
         paint: {
-            'fill-color': '#ffa500',
+            'fill-color': ['get', 'feature_color'],
             'fill-opacity': 0.6
+        }
+    };
+    const runwaysLabelLayer = {
+        id: 'runways_label',
+        type: 'symbol',
+        'source-layer': 'runways',
+        layout: {
+            'text-field': ['get', 'designator'],
+            'text-size': 12,
+            'text-justify': 'center'
+
+        },
+        paint: {
+            'text-color': '#000000'
         }
     };
     const countriesLayer = {
@@ -84,8 +94,9 @@ const AddressTable = () => {
         type: 'line',
         'source-layer': 'ne_10m_admin_0_countries',
         paint: {
-            'line-color': '#ff006a',
-            'line-width': 2
+            'line-color': '#808080',
+            'line-width': 2,
+            'line-dasharray': [2, 2]
         }
     };
     const railroadsLayer = {
@@ -98,13 +109,37 @@ const AddressTable = () => {
         }
     };
 
+    const osmRasterStyle = {
+        version: 8,
+        sources: {
+            'osm-raster': {
+                type: 'raster',
+                tiles: [
+                    'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+                ],
+                tileSize: 256,
+                attribution: '&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+            }
+        },
+        layers: [
+            {
+                id: 'osm-raster-layer',
+                type: 'raster',
+                source: 'osm-raster',
+                minzoom: 0,
+                maxzoom: 19
+            }
+        ]
+    }
+
     if (isLoading) {
         return <div>Loading...</div>;
     } else if (isError) {
         return <div>Failed to load the addresses</div>;
     }
+
     return (
-        <div className="address-container">
+        <div className="address-container" >
             <Box>
                 <PopupState variant="popover" popupId="customer-popup-menu">
                     {(popupState) => (
@@ -129,42 +164,45 @@ const AddressTable = () => {
                         </React.Fragment>
                     )}
                 </PopupState>
-                <DataGrid checkboxSelection={true}
-                    rows={addresses}
-                    columns={addressColumns}
-                    onRowSelectionModelChange={(newRowSelectionModel) => {
-                        dispatch(setSelections([...newRowSelectionModel.ids]))
-                    }}
-                    disableRowSelectionExcludeModel={true}
-                />
+                <div style={{ height: 375, width: '100%' }}>
+                    <DataGrid checkboxSelection={true}
+                        rows={addresses}
+                        columns={addressColumns}
+                        onRowSelectionModelChange={(newRowSelectionModel) => {
+                            dispatch(setSelections([...newRowSelectionModel.ids]))
+                        }}
+                        disableRowSelectionExcludeModel={true}
+                    />
+                </div>
                 <Address forceUpdate={refetch} />
                 {(selectionCount == 1) && <Map
+                    ref={mapRef}
+                    onClick={getRawTileData}
                     initialViewState={{
                         longitude: addresses.find((address) => address.id == selections[0])?.coordinates.coordinates[0],
                         latitude: addresses.find((address) => address.id == selections[0])?.coordinates.coordinates[1],
                         zoom: 12
                     }}
                     style={{
-                        width: "50%", height: 400, margin: '0 auto'
+                        width: "50%", height: 375, margin: '0 auto'
                     }}
                     mapLib={maplibregl}
-                    mapStyle="https://demotiles.maplibre.org/style.json"
+                    mapStyle={osmRasterStyle}
                 >
                     <Source
                         id="martin-nj-pmtiles"
                         type="vector"
                         tiles={['http://localhost:3000/new-jersey/{z}/{x}/{y}']}
                     >
-                        <Layer {...transportationLayer} />
-                        <Layer {...parksLayer} />
                         <Layer {...waterLayer} />
                     </Source>
                     <Source
                         id="postgis-runways"
                         type="vector"
-                        tiles={['http://localhost:3001/runways_suitable/{z}/{x}/{y}?aircraft=C17']}
+                        tiles={['http://localhost:3000/runways_suitable/{z}/{x}/{y}?aircraft=C17']}
                     >
                         <Layer {...runwaysLayer} />
+                        <Layer {...runwaysLabelLayer} />
                     </Source>
                     <Source
                         id="countries"
@@ -184,7 +222,7 @@ const AddressTable = () => {
                 </Map>}
 
             </Box>
-        </div>
+        </div >
     );
 };
 
